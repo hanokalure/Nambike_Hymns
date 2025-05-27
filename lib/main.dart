a5dd0f10d91fe66061f0e2a83d56263e6e233dbd
Hanok Alure <hanokalure@gmail.com>
2025-05-28 00:00:29 +0530

    updated name from Prince of Peace to Nambike Hymns

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Prince of Peace Songs',
+      title: 'Nambike Hymns',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomeScreen(),
     );
